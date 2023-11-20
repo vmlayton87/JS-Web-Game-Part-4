a5c0e2f8a0a51f44fc1415da7ddbc20d05d66b30
vmlayton87 <vmlayton87@gmail.com>
2023-11-19 19:58:17 -0500

    solution NO bonus

--- move.js
+++ move.js
@@ -6,7 +6,39 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left, bottom, onDirectionChange) {
+        let direction = null
+        let x = left
+        let y = bottom
+
+        function moveCharacter() {
+        if (direction === 'north') {y = y + 1}
+        if (direction === 'south') {y = y - 1}
+        if (direction === 'east') {x = x + 1}
+        if (direction === 'west') {x = x - 1}
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        }    
+        setInterval(moveCharacter, 1)
+
+
+        document.addEventListener('keydown', function(event){
+        if (event.repeat) return
+        if (event.key === 'ArrowUp') {direction = 'north'}
+        if (event.key === 'ArrowDown') {direction = 'south'}
+        if (event.key === 'ArrowRight') {direction = 'east'}
+        if (event.key === 'ArrowLeft') {direction = 'west'}
+        onDirectionChange(direction)
+        })
+
+        document.addEventListener('keyup', function(event){
+            direction = null
+            onDirectionChange(direction)})
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates, 
+        withArrowKeys: moveWithArrowKeys
     }
 }
